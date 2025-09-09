08885023199fd5e99c95bb2765b7f668246fd65f
dev035 <sosiwon@naver.com>
2025-09-09 15:26:30 +0800

    explorer tt

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -9,7 +9,7 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   return (
    <h2 class={classNames(displayClass, "page-title")}>
   <a href={baseDir}>
-    <img src="/_etc/lx2.png" alt={title} height="40" />
+    <img src="/_etc/lx2.png" alt={title} height="150" />
   </a>
 </h2>
   )
