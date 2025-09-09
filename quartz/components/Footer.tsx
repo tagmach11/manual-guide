86bc2e5dacc406b4b1a6f204b1ce8265bbd6011c
dev035 <sosiwon@naver.com>
2025-09-09 14:42:55 +0800

    explorer tt

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -16,7 +16,7 @@ export default ((opts?: Options) => {
         <p>
         
           <a href="https://lx2.4csoft.com/">
-  <img src="/_etc/lx2_logo2.png" alt="LX2 로고" height="32" />
+  <img src="/_etc/lx2_logo2.png" alt="LX2 로고" height="25" />
 </a>
 </p>
         <ul>
