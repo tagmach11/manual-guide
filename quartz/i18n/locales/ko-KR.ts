c8ce5002c9ad062a1d992f67a61d642d5498f6b3
dev035 <sosiwon@naver.com>
2025-09-09 13:33:51 +0800

    explorer tt

--- quartz/i18n/locales/ko-KR.ts
+++ quartz/i18n/locales/ko-KR.ts
@@ -57,7 +57,7 @@ export default {
       title: "목차",
     },
     contentMeta: {
-      readingTime: ({ minutes }) => `${minutes} 읽음`,
+      readingTime: ({ minutes }) => `${minutes}분 전에 읽음`,
     },
   },
   pages: {
